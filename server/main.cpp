bdf40e73a527446e69f7d452ae18e78e5a8da0f7
c0ldlasagna <erechoum@gmail.com>
2025-05-20 21:24:12 +0700

    fixed main

--- server/main.cpp
+++ server/main.cpp
@@ -1,19 +1,6 @@
 #include "log.hpp"
 #include "Server.hpp"
 
-#include <csignal>
-#include <atomic>
-
-std::atomic<bool> running(true);
-listenServer* globalServer = nullptr;
-
-void signalHandler(int signum) {
-    if (globalServer) {
-        globalServer->shutdown();
-    }
-    running = false;
-}
-
 int main(){
     listenServer srv("127.0.0.1",6969);
 
